786a71a946a623c383d698e192ebeebc62ef88df
zhangxuechun <258613612@qq.com>
2017-11-04 16:45:38 +0800

    初始版 无样式

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,21 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show]
+
+  # GET /categories
+  # GET /categories.json
+  def index
+    @categories = Category.where(is_del: 0)
+  end
+
+  # GET /categories/1
+  # GET /categories/1.json
+  def show
+    @products = @category.products.where(is_del: 0).paginate(page: params[:page], per_page: 10).order(updated_at: :desc)
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+end
